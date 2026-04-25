f912cb1d5c4f0e92eb54b51d1ef843232a60aa7f
Ritav <ritav08092004@gmail.com>
2026-04-25 06:12:03 +0530

    Update Design.v

--- Design.v
+++ Design.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 22.04.2026 10:25:36
-// Design Name: 
-// Module Name: Tff
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module Tff(
    input T_i, 
